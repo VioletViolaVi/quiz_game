e1120d63710a7d83cf73238c09552dd4cd7324d7
VioletViolaVi <vivian.v18723@gmail.com>
2022-05-08 00:52:02 +0100

    show correct option when wrong answer selected

--- scripts.js
+++ scripts.js
@@ -1,52 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // returns answers to questions
+  function allQuestionsAndAnswers() {
+    const questionAndAnswer = {
+      question: "What colour added to yellow makes green?",
+      answer: "blue",
+    };
+    return questionAndAnswer;
+  }
+
   // gets selected choice
-  const selectedAnswer = () => {
+  function selectedAnswer() {
     // stores arr of options
-    const allOptionsArr = document.getElementsByClassName('options')
+    const allOptionsArr = document.getElementsByClassName("options");
 
     // selects 1 option
     for (let i = 0; i < allOptionsArr.length; i++) {
       // single option
-      const singleOption = allOptionsArr[i]
+      const singleOption = allOptionsArr[i];
 
-      // answers to questions function
-      allQuestionsAndAnswers()
+      // calls function
+      allQuestionsAndAnswers();
 
-      singleOption.addEventListener('click', () => {
+      singleOption.addEventListener("click", () => {
         // change option's bg colour
         if (singleOption.innerHTML === allQuestionsAndAnswers().answer) {
-          singleOption.style.backgroundColor = '#3fff00'
+          singleOption.style.backgroundColor = "#3fff00";
         } else {
-          singleOption.style.backgroundColor = '#ff0800'
+          singleOption.style.backgroundColor = "#ff0800";
+          document.getElementById("optionA").style.backgroundColor = "#3fff00";
         }
-      })
+      });
     }
   }
-  selectedAnswer()
-
-  function allQuestionsAndAnswers() {
-    const questionAndAnswer = {
-      question: 'What colour added to yellow makes green?',
-      answer: 'blue',
-    }
-    return questionAndAnswer
-  }
+  selectedAnswer();
 
   // clears answer for next question
-  const nextQuestion = () => {
-    const nextBtn = document.getElementById('nextBtn')
-    const optionA = document.getElementById('optionA')
-    const optionB = document.getElementById('optionB')
-    const optionC = document.getElementById('optionC')
-    const optionD = document.getElementById('optionD')
+  function nextQuestion() {
+    const nextBtn = document.getElementById("nextBtn");
+    const optionA = document.getElementById("optionA");
+    const optionB = document.getElementById("optionB");
+    const optionC = document.getElementById("optionC");
+    const optionD = document.getElementById("optionD");
 
-    nextBtn.addEventListener('click', () => {
-      optionA.style.backgroundColor = '#ffffe0'
-      optionB.style.backgroundColor = '#ffffe0'
-      optionB.style.backgroundColor = '#ffffe0'
-      optionC.style.backgroundColor = '#ffffe0'
-      optionD.style.backgroundColor = '#ffffe0'
-    })
+    nextBtn.addEventListener("click", () => {
+      optionA.style.backgroundColor = "#ffffe0";
+      optionB.style.backgroundColor = "#ffffe0";
+      optionB.style.backgroundColor = "#ffffe0";
+      optionC.style.backgroundColor = "#ffffe0";
+      optionD.style.backgroundColor = "#ffffe0";
+    });
   }
-  nextQuestion()
-})
+  nextQuestion();
+});
